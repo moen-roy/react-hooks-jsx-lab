fb0ba731a8c599418ce15fe865182b6e1fa0a5e7
moen <moenr49@gmail.com>
2025-07-17 08:45:03 +0300

    Enhance About component with profile details and image

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>This is my profile and i hope it works. if you are seeing this, then i guess it does work</p>
+    <img src={image} alt="I made this" />
+  </div>
+  )
 }
 
 export default About;
